879b2ebd38c518ab22a2254714aed04e01cf442a
Mohammad Taha <mohammad.taha5@hotmail.com>
2023-07-09 04:42:16 +0300

    image fetching

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,6 +1,9 @@
 "use client";
+import Image from "next/image";
 
+import useLoudImage from "@/hooks/useLoudImage";
 import { Song } from "@/types";
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
   data: Song;
@@ -8,9 +11,86 @@ interface SongItemProps {
 }
 
 const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
+  const imagePath = useLoudImage(data);
   return (
     <>
-      <div>Songs Item</div>
+      <div
+        onClick={() => onClick(data.id)}
+        className="
+          relative
+          group
+          flex
+          flex-col
+          items-center
+          justify-center
+          rounded-md
+          overflow-hidden
+          gap-x-4
+          bg-neutral-400/5
+          cursor-pointer
+          hover:bg-neutral-400/10
+          transition
+          p-3
+        "
+      >
+        <div
+          className="
+            relative
+            aspect-square
+            w-full
+            h-full
+            rounded-md
+            overflow-hidden
+          "
+        >
+          <Image
+            className="object-cover"
+            src={imagePath || "/images/liked.png"}
+            fill
+            alt="Image"
+          />
+        </div>
+        <div
+          className="
+            flex 
+            flex-col 
+            items-start 
+            w-full 
+            pt-4 
+            gap-y-1
+          "
+        >
+          <p
+            className="
+              font-semibold
+              truncate
+              w-full 
+            "
+          >
+            {data.title}
+          </p>
+          <p
+            className="
+              text-neutral-400
+              text-sm
+              pb-4
+              w-full 
+              truncate
+            "
+          >
+            By {data.author}
+          </p>
+        </div>
+        <div
+          className="
+            absolute
+            bottom-24
+            right-5
+          "
+        >
+          <PlayButton />
+        </div>
+      </div>
     </>
   );
 };
